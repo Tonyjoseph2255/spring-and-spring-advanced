20e4f2732ab1db8fae81dd6f65bacef26d61f8ab
Tonyjoseph2255 <101250944+Tonyjoseph2255@users.noreply.github.com>
2022-04-01 11:10:11 +0530

    Add files via upload

---
+++ springcore/properties/PropertiesTest.java
@@ -0,0 +1,14 @@
+package com.ust1.spring.springcore.properties;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class PropertiesTest {
+
+	public static void main(String[] args) {
+		ApplicationContext ac=new ClassPathXmlApplicationContext("com/ust1/spring/springcore/properties/proprtiesconfig.xml");
+		CountriesAndLanguages cal=(CountriesAndLanguages) ac.getBean("cAndL");
+		System.out.println(cal);// to string method is invoked automatically
+	}
+
+}
